df3301be70e97e95ba45b82774291278535452c7
Debbie Gray <dgray@deborahgraydesign.com>
2013-10-03 10:48:13 -0700

    modifies footer

--- footer.php
+++ footer.php
@@ -13,7 +13,6 @@
 				<small>&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?></small>
 				<p>Brought to you by the good folks at Twitter and Zurb and cobbled together by the UX team at Digital First Media.</p>
 				<p>Original project links:<span class="divider"></span><a href="http://getbootstrap.com/">Bootstrap</a> <span class="divider">|</span> <a href="http://foundation.zurb.com/">Foundation</a><span class="divider">|</span><a href="http://sandbox.digitalfirstmedia.com/strapit/">Strapit Documentation</a></p>
-				<p>Code licensed under <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License v2.0</a>, documentation under <a href="http://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>.</p>
 			    <ul class="footer-links">
 			      <li>Currently v2.0.0</li>
 			      <!-- <li class="muted">&middot;</li>
